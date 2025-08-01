921f2149b44c9270bddff05e1e4dac2cf007cbd1
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-08-01 16:17:22 +0700

    update

---
+++ modules/toggle-table.js
@@ -0,0 +1,20 @@
+const tableToggles = document.querySelectorAll('.tr-header');
+
+tableToggles.forEach(row => {
+    row.addEventListener('click', () => {
+        const parentRow = row.closest('.main-tr');
+        if (parentRow) {
+            parentRow.classList.toggle('open');
+        }
+    });
+});
+const faqToggles = document.querySelectorAll('.ques-heading');
+
+faqToggles.forEach(row => {
+    row.addEventListener('click', () => {
+        const parentRow = row.closest('.ques-li');
+        if (parentRow) {
+            parentRow.classList.toggle('open');
+        }
+    });
+});
