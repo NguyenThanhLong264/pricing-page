04beaaa23e1ec3e4def278723c84a4eba6ab7b96
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-08-05 11:57:39 +0700

    scroll

--- modules/mobile-layout.js
+++ modules/mobile-layout.js
@@ -1,14 +1,13 @@
 function applyResponsiveLayout() {
-    const width = window.innerWidth;
+    const width = document.documentElement.clientWidth;
+    console.log(width);
 
     if (width < 768) {
+        renderMobileLayout(features);
     } else {
+        renderLaptopLayout(features)
     }
 }
 
 window.addEventListener('load', applyResponsiveLayout);
 window.addEventListener('resize', applyResponsiveLayout);
-
-function layoutMobileFeatures() {
-
-}
