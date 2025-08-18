0abbff0863201cfdcd7417895efdd1308bf767e0
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-08-18 21:16:44 +0700

    bỏ contact

--- modules/form.js
+++ modules/form.js
@@ -9,7 +9,7 @@ function displayForm() {
                     </svg>
                 </button>
                 <div class="form-content">
-                    <form action="contact">
+                    <form id="contact-form">
                         <div class="form-title">
                             <h3 class="form-title-text">Đăng ký tư vấn ngay</h3>
                             <p class="normal-text">Chúng tôi sẵn sàng tư vấn giải pháp phù hợp nhất với mô hình hoạt
@@ -21,7 +21,7 @@ function displayForm() {
                                 <div class="input-content">
                                     <label class="input-label" for="name">Họ Tên</label>
                                     <div class="input-detail">
-                                        <input type="text" id="name" name="name" required="">
+                                        <input type="text" id="name" name="name" required>
                                         <div class="input-icon">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                                 fill="currentColor" viewBox="0 0 16 16">
@@ -38,7 +38,7 @@ function displayForm() {
                                                 d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m0 9.5a.5.5 0 0 1-.5-.5V4.5a.5.5 0 0 1 1 0V9a.5.5 0 0 1-.5.5m0 1A.75.75 0 1 1 8 12a.75.75 0 0 1 0-1.5"
                                                 clip-rule="evenodd"></path>
                                         </svg>
-                                        This field is required
+                                        Vui lòng nhập trường này
                                     </div>
                                 </div>
                             </div>
@@ -63,7 +63,7 @@ function displayForm() {
                                                 d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m0 9.5a.5.5 0 0 1-.5-.5V4.5a.5.5 0 0 1 1 0V9a.5.5 0 0 1-.5.5m0 1A.75.75 0 1 1 8 12a.75.75 0 0 1 0-1.5"
                                                 clip-rule="evenodd"></path>
                                         </svg>
-                                        This field is required
+                                        Vui lòng nhập trường này
                                     </div>
                                 </div>
                             </div>
@@ -113,7 +113,7 @@ function displayForm() {
                                                 d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m0 9.5a.5.5 0 0 1-.5-.5V4.5a.5.5 0 0 1 1 0V9a.5.5 0 0 1-.5.5m0 1A.75.75 0 1 1 8 12a.75.75 0 0 1 0-1.5"
                                                 clip-rule="evenodd"></path>
                                         </svg>
-                                        This field is required
+                                        Vui lòng nhập trường này
                                     </div>
                                 </div>
                             </div>
@@ -138,7 +138,7 @@ function displayForm() {
                                                 d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m0 9.5a.5.5 0 0 1-.5-.5V4.5a.5.5 0 0 1 1 0V9a.5.5 0 0 1-.5.5m0 1A.75.75 0 1 1 8 12a.75.75 0 0 1 0-1.5"
                                                 clip-rule="evenodd"></path>
                                         </svg>
-                                        This field is required
+                                        Vui lòng nhập trường này
                                     </div>
                                 </div>
                             </div>
@@ -162,14 +162,14 @@ function displayForm() {
                             </div>
                         </div>
                         <div class="form-submit">
-                            <a href="#" class="primary-btn">
+                            <button class="primary-btn" type="submit">
                                 <span class="btn-text-bg">
                                     <span class="btn-span">
                                         <span class="i-text">Contact Sales</span>
                                         <span class="af-text">Contact Sales</span>
                                     </span>
                                 </span>
-                            </a>
+                            </button>
                         </div>
                     </form>
                 </div>
@@ -226,6 +226,20 @@ function displayForm() {
                             if (icon) icon.style.display = 'none';
                         }
                     });
+                });
+
+                const form = document.getElementById('contact-form')
+                form.addEventListener('submit', (e) => {
+                    e.preventDefault(); // ngăn form gửi đi
+                    const data = {
+                        name: form.name.value.trim(),
+                        company: form.company.value.trim(),
+                        email: form.email.value.trim(),
+                        phone: form.phone.value.trim(),
+                        message: form.message.value.trim(),
+                        agree: form.agree.checked
+                    };
+                    console.log(data);
                 });
             }
         };
